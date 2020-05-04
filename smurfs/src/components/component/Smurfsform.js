0e8543adedf13771cd753098ea62a05b7c5c473c
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-03 20:58:50 -0500

    Started building SmurfsForm

---
+++ smurfs/src/components/component/Smurfsform.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import { connect } from "react-redux";
+import { Form } from "react-bootstrap";
+
+const SmurfsForm = () => {
+    return(
+        <Form>
+            
+        </Form>
+    );
+}
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, {})(SmurfsForm);

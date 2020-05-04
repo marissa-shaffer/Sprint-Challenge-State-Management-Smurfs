944cf631fac1a45c279d13e00098c6d93201aa36
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-03 19:49:58 -0500

    set up imports and store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {Provider} from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { reducer } from "./components/store/reducer/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , rootElement);

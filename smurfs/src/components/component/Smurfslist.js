a07250ca501f6a9d7feb5f82bc7dad9cef7433b8
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-03 19:50:51 -0500

    Started building SmurfsList comp & connect to app

---
+++ smurfs/src/components/component/Smurfslist.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SmurfsList = () => {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+const mapStateToProps = state => {
+    return {
+        
+    };
+};
+
+export default connect (mapStateToProps, {}) (SmurfsList);

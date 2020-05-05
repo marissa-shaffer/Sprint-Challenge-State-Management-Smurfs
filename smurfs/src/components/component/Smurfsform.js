3adae92a9b7b2a93f4de3e3e9c1f45658eae75dc
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-05 07:48:58 -0500

    built form in SmurfsForm

--- smurfs/src/components/component/Smurfsform.js
+++ smurfs/src/components/component/Smurfsform.js
@@ -3,9 +3,43 @@ import { connect } from "react-redux";
 import { Form } from "react-bootstrap";
 
 const SmurfsForm = () => {
+
+    const handleChange = e => {
+        e.preventDefault();
+        setInput({...InputDeviceInfo, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        addSmurf(input);
+        setInput(inputState);
+    }
+        
+
     return(
-        <Form>
-            
+        <Form onSubmit={handleSubmit}>
+            <h2>Name a Smurf</h2>
+            <input
+                name="name"
+                value={input.name}
+                placeholder="Name"
+                onChange={handleChange}
+            />
+            <h2>How Old Are They?</h2>
+            <input 
+                name="age"
+                value={input.age}
+                placeholder="Age"
+                onChange={handleChange}
+            />
+            <h2>How Tall Are They?</h2>
+            <input 
+                name="height"
+                value={input.height}
+                placeholder="Height"
+                onChange={handleChange}
+            />
+            <button type="submit" value="submit">Submit</button>
         </Form>
     );
 }

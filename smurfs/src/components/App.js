968f5f55e928c9d0e9257bf0c19b0a691a58722b
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-05 17:58:50 -0500

    Added SmurfsForm Component to app

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import SmurfsList from "./component/SmurfsList";
+import SmufsForm from "./component/SmurfsForm";
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <h1>SMURFS!</h1>
         <SmurfsList />
+        <SmurfsForm />
       </div>
     );
   }
